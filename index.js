56baf0beb6152cc906d2b2ebf2fd5a149230f2e0
PPS <pavlina.psarsky@gmail.com>
2022-04-06 09:35:04 +0200

    desktop layout done, one answer opens at a time

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ const answers = document.querySelectorAll(".FAQ-card__accordion__answer");
 
 // ***only one answer open at a time***
 
-// questions.forEach(question => question.addEventListener('click', (e) => {
+questions.forEach(question => question.addEventListener('click', (e) => {
     
-//     questions.forEach(item => {
-//         item.setAttribute('data-visible', false);
-//         item.nextElementSibling.setAttribute('data-visible', false);
-//     })
+    questions.forEach(item => {
+        item.setAttribute('data-visible', false);
+        item.nextElementSibling.setAttribute('data-visible', false);
+    })
 
-//     e.target.setAttribute('data-visible', true);
-//     e.target.nextElementSibling.setAttribute('data-visible', true);
-// } ));
+    e.target.setAttribute('data-visible', true);
+    e.target.nextElementSibling.setAttribute('data-visible', true);
+} ));
 
 
 // ***all answers can stay open until clicked again***
 
-questions.forEach(question => question.addEventListener('click', (e) => {
-    const visibility = e.target.getAttribute('data-visible');
-    if(visibility === 'false') {
-        e.target.setAttribute('data-visible', true);
-        e.target.nextElementSibling.setAttribute('data-visible', true);
-    } else {
-        e.target.setAttribute('data-visible', false);
-        e.target.nextElementSibling.setAttribute('data-visible', false);
-    }
-} ));
+// questions.forEach(question => question.addEventListener('click', (e) => {
+//     const visibility = e.target.getAttribute('data-visible');
+//     if(visibility === 'false') {
+//         e.target.setAttribute('data-visible', true);
+//         e.target.nextElementSibling.setAttribute('data-visible', true);
+//     } else {
+//         e.target.setAttribute('data-visible', false);
+//         e.target.nextElementSibling.setAttribute('data-visible', false);
+//     }
+// } ));
